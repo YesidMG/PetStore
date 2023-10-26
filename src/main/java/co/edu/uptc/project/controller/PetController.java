d2ab09726d9c794d0018973821a00346af91bf9a
JLC <juanlopezcastrods@gmail.com>
2023-10-25 22:24:23 -0500

    se agrego metodo buscar mascota

--- src/main/java/co/edu/uptc/project/controller/PetController.java
+++ src/main/java/co/edu/uptc/project/controller/PetController.java
@@ -1,9 +1,11 @@
 package co.edu.uptc.project.controller;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import co.edu.uptc.project.service.Services;
@@ -15,6 +17,11 @@ public class PetController {
 
 	@Autowired
 	private Services service;
+
+	@GetMapping(path = "/{id}")
+	public Optional<Pet> getPetById (@PathVariable int id){
+		return this.service.getPetById(id);
+	}
 
 	@GetMapping
 	public ArrayList<Pet> getPets(){
